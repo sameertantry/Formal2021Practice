ea82b7e6f8fde04d816ca7d85f7da8d1c5f84391
Sameer Tantry <sameertantry@gmail.com>
2021-11-30 02:42:03 +0300

    Tests

---
+++ tests/CMakeFiles/Test.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/sameertantry/Desktop/stuff/proga/formal_lang_practice/tests/RecognizeTestCase.cpp" "CMakeFiles/Test.dir/RecognizeTestCase.cpp.o" "gcc" "CMakeFiles/Test.dir/RecognizeTestCase.cpp.o.d"
+  "/Users/sameertantry/Desktop/stuff/proga/formal_lang_practice/CYK.cpp" "CMakeFiles/Test.dir/Users/sameertantry/Desktop/stuff/proga/formal_lang_practice/CYK.cpp.o" "gcc" "CMakeFiles/Test.dir/Users/sameertantry/Desktop/stuff/proga/formal_lang_practice/CYK.cpp.o.d"
+  "/Users/sameertantry/Desktop/stuff/proga/formal_lang_practice/tests/main.cpp" "CMakeFiles/Test.dir/main.cpp.o" "gcc" "CMakeFiles/Test.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tests/CMakeFiles/Test.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test.dir/RecognizeTestCase.cpp.o"
+  "CMakeFiles/Test.dir/RecognizeTestCase.cpp.o.d"
+  "CMakeFiles/Test.dir/Users/sameertantry/Desktop/stuff/proga/formal_lang_practice/CYK.cpp.o"
+  "CMakeFiles/Test.dir/Users/sameertantry/Desktop/stuff/proga/formal_lang_practice/CYK.cpp.o.d"
+  "CMakeFiles/Test.dir/main.cpp.o"
+  "CMakeFiles/Test.dir/main.cpp.o.d"
+  "Test"
+  "Test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
